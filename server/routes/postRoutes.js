05e72af8d4aa2a3a503643a8d35d4e844d4fe905
Mehmet Rıdvan Gül <mgul440@hotmail.com>
2024-02-15 15:12:46 +0300

    Update: postRoutes.js

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,17 +1,16 @@
-const express = require('express');
-const multer = require('multer');
-const postController = require('../controllers/postController');
+const express = require("express");
+const multer = require("multer");
+const postController = require("../controllers/postController");
 
 const router = express.Router();
 
-const upload = multer({dest:'public/'})
+const upload = multer({ dest: "public/" });
 
-router.post('/addPost',upload.single('photo'),postController.addPost);
-router.get('/getPost', postController.getPost);
-router.post('/getFollowedUsersPosts', postController.getFollowedUsersPosts);
-router.delete('/deletePost/:id', postController.deletePost);
-router.post('/posts/:postId/like',postController.postLike);
-router.post('/posts/:postId/unlike',postController.postUnLike);
-
+router.post("/addPost", upload.single("photo"), postController.addPost);
+router.get("/getPost", postController.getPost);
+router.post("/getFollowedUsersPosts", postController.getFollowedUsersPosts);
+router.delete("/deletePost/:id", postController.deletePost);
+router.post("/posts/:postId/like", postController.postLike);
+router.post("/posts/:postId/unlike", postController.postUnLike);
 
 module.exports = router;
